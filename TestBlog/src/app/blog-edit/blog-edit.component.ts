bb2b3df3220603b4f8e9c35a6db8aedbe932a8d7
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-14 01:17:46 +0700

    update Update function

--- TestBlog/src/app/blog-edit/blog-edit.component.ts
+++ TestBlog/src/app/blog-edit/blog-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Blog} from "../blog";
 import {BlogService} from "../blog.service";
 import {ActivatedRoute} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-blog-edit',
@@ -11,19 +12,39 @@ import {ActivatedRoute} from "@angular/router";
 export class BlogEditComponent implements OnInit {
 
   blog: Blog;
+  blogForm: FormGroup = new FormGroup({
+    id: new FormControl(''),
+    userId: new FormControl(''),
+    title: new FormControl(''),
+    body: new FormControl(''),
+  });
+   message: string;
+  private blogId: any;
 
   constructor(private blogService: BlogService,
               private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    const id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.blogService.getById(id).subscribe(
-      next => (this.blog = next),
-      error => {
-        console.log(error);
-        this.blog = null;
+    this.route.params.subscribe(params => {
+      this.blogId = params.id;
+      this.blogService.getById(this.blogId).subscribe(result => {
+        this.blogForm.setValue(result);
       });
+    });
   }
 
+  onSubmit() {
+    if (this.blogForm.valid) {
+      const {value} = this.blogForm;
+      this.blogService.updateBlog(value)
+        .subscribe(next => {
+          this.blogForm.reset({
+            title: '',
+            body: ''
+          });
+          this.message="SAVED!"
+        }, error => console.log(error));
+    }
+  }
 }
